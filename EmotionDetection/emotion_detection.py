4d6d5ea54ccb815b2413fb647bd5cac97533965c
Stephen Winata <digital.engineer@outlook.com>
2025-05-17 08:48:49 -0400

    Incorporate error handling for invalid input text

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,13 +14,15 @@ def emotion_detector(text_to_analyse):
     # Make a POST request to the API with the headers and payload
     response = requests.post(url, headers=header, json=payload)
 
-    # Parse the response from the API
-    formatted_response = json.loads(response.text)
+    # If the response status code is 200, extract the emotions from the response
+    if response.status_code == 200:
+      # Parse the response from the API
+      formatted_response = json.loads(response.text)
+      # Get the dominant emotion
+      emotions = formatted_response['emotionPredictions'][0]['emotion']
+      dominant_emotion = max(emotions, key=emotions.get)
+      emotions['dominant_emotion'] = dominant_emotion
+    else:
+      emotions = {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
 
-    # Get the dominant emotion
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-
-    emotions['dominant_emotion'] = dominant_emotion
     return emotions
-
